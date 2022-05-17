e4251172a3535cd9346592d644d8c88797cf52dc
minhaejo <robo888@naver.com>
2022-05-17 00:31:46 -0700

    seventeenth commit

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -1,3 +1,45 @@
+//함수인자 인터페이스 활용
+interface User {
+    age:number
+    name:string
+}
+
+var seho :User = {
+    age:33,
+    name:"세호"
+}
+function getUser(user:User) {
+    console.log(user)
+}
+const capt = {
+    name:"캡틴",
+    age:30
+}
+getUser(capt)
+
+
+
+//함수의 스펙에 인터페이스 활용
+interface SumFunction {
+(a:number,b:number):number
+}
+var sum :SumFunction
+sum = function (a:number ,b:number) {
+    return a+b
+}
+
+
+
+//인덱싱
+
+interface StringArray {
+    [index:number]:string
+}
+var arr :StringArray =["a","b","c"]
+arr[0] = 10
+
+
+
 //딕셔너리 패턴
 
 interface stringRegexDictionary {
@@ -8,7 +50,11 @@ var obj:stringRegexDictionary = {
     cssFile :/\.css$/,
     jsFile : /\.js$/
 }
-Object.keys(obj)
+obj["cssFile"] = "a"
+Object.keys(obj).forEach((value)=>{
+
+
+})
 
 
 
@@ -23,9 +69,9 @@ interface Developer extends Person {
     language:string
 }
 
-var capt:Developer ={
-    language:"hi",
-    age:100,
-    name:"minhae"
+// var capt:Developer ={
+//     language:"hi",
+//     age:100,
+//     name:"minhae"
 
-}
+// }
